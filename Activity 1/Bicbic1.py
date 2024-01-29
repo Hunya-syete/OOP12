73dcc67c41fcb08dcc90c0e05cfb977ce0d2f7d8
Jhon <158005993+banneee@users.noreply.github.com>
2024-01-29 11:24:12 +0800

    Create Bicbic1.py

---
+++ Activity 1/Bicbic1.py
@@ -0,0 +1,54 @@
+# x = int(100)
+# print("Number is " + str(x))
+
+
+# x = "20"
+# y = "10"
+# print(int(x) + int(y))
+
+#---
+
+# name = input("Enter Full Name: ")
+# email = input("Enter Email: ")
+# print ("Name: " + name)
+# print ("Email: " + email)
+
+#---
+
+# name = "Jhon"
+# food = "fries"
+# game = "PC"
+#
+# sampleText1 = "My name is {} I love {} and playing {}. "
+# sampleText1a = sampleText1.format(name, food, game)
+# print(sampleText1a)
+#
+# sampleText2 = "My name is {2} I love {1} and playing {0}. "
+# sampleText2a = sampleText2.format(name, food, game)
+# print(sampleText2a)
+#
+# sampleText3 = "My name is {newname} I love {newfood} and playing {newgame}. "
+# sampleText3a = sampleText3.format(newname="Jack", newfood="ice cream", newgame="outside")
+# print(sampleText3a)
+
+# ---
+
+# item = "juice"
+# cost = 19.977
+#
+# sampleText4 = "The product %s costs %.2f"  % (item, cost)
+# print(sampleText4)
+
+#---
+
+import math
+grade1 = 95.71
+grade2 = 95.39
+
+print(round(grade1,1))
+print(math.ceil(grade1))
+print(math.ceil(grade2))
+print(math.floor(grade1))
+print(math.floor(grade2))
+print(pow(2,3))
+print(2**3)

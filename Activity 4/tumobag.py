4d76b8b59a9dd45894778c23b65092139f29e8a6
Geloo <158006326+gelotumobag@users.noreply.github.com>
2024-02-05 10:29:43 +0800

    tumobag.py

---
+++ Activity 4/tumobag.py
@@ -0,0 +1,8 @@
+# Generate a number
+num = int(input("Enter a Number: "))
+
+# Generate and Print Multiplication table
+print(f"Multiplication table for {num}:")
+for i in range(1,10):
+    result = num * i
+    print(f"{num} x {i} = {result}")

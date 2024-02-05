f4ac017d7901be7a0ff4ac7ab5e57860bbb946d7
Vincel May Empeynado <158006073+beansyl@users.noreply.github.com>
2024-02-05 10:11:49 +0800

    empeynadoact4.py

---
+++ Activity 4/empeynadoact4.py
@@ -0,0 +1,13 @@
+task1 = int(input("Enter a number from 1-10 to generate a multiplication table: "))
+mult = 1
+
+while mult <= 10:
+    print(str(task1) + "x" + str(mult) + "=" + str(task1 * mult))
+
+    mult += 1
+
+
+
+task1 <= 10
+if task1 >10:
+     print("Invalid number!")

c40b1ea360e5f74450b9959ef155d1f68565df99
JadeGLabin <158074280+JadeGLabin@users.noreply.github.com>
2024-02-05 14:18:17 +0800

    Create rodel juguilon act4.py

---
+++ Activity 4/rodel juguilon act4.py
@@ -0,0 +1,10 @@
+table1 = float(input("enter the number count range from 1- 10 to generate multiplication table: "))
+multiplied = 1
+
+while multiplied <= 10:
+    print(str(table1) + "x" + str(multiplied) + "=" + str(table1 * multiplied))
+    multiplied += 1
+
+table1 <= 10
+if table1 >10:
+      print ("the number count range from 1- 10 ")

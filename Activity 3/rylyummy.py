abf0752d1cfa6a9da7f7c5b715b3c4d307de1c1c
realyummy <158006120+therylryan@users.noreply.github.com>
2024-02-12 09:55:03 +0800

    rylyummy.py

---
+++ Activity 3/rylyummy.py
@@ -0,0 +1,21 @@
+movie_mRating = input("Rating of the Movie?:")
+actionmovie = "the hungry games "
+funnymovie = "home alone "
+horrormovie = "insidious the red door"
+recommendation = "(movie_genre)"
+diff_genre = "Action, Comedy, or thriller"
+
+print (f"Genre: Movie_Genre")
+print (f"Rating: Movie_ Rating")
+
+
+if "movie" == "action" and "Rating" > 8:
+    print(f"Recommended Action Movie: (Action movie")
+
+if "movie" == "comedy" and "Rating" > 7:
+    print(f"Recommended Action Movie: (Funny movie")
+
+if "movie"=="romance" and "Rating" > 6:
+    print (f"Recommended Action Movie: (Horror movie")
+else:
+    print(f"Genre, Recommended: \n(recommended")

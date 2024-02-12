42c4a1cbcbfec3417257c550a06a5067765b548b
Hunya-syete <tinangmarcillones@gmail.com>
2024-02-12 10:26:00 +0800

    Please pass today

---
+++ Activity 6/Jumawan.py
@@ -0,0 +1,15 @@
+# Create a Record Keeping App
+# The application will ask the user to choose between:
+# Add Data
+# Delete Data
+# End
+# If Add data, the application will ask the user to input key and its value
+# Enter Key: Lastname
+# Enter Value: Doe
+# Store the information in a dictionary
+# Display the result
+# If Delete Data, the application will ask for the key
+# Enter Key: Lastname
+# Remove the item from the dictionary
+# Display the result
+# If End, display THANK YOU
---
+++ Activity 7/Jumawan.py
@@ -0,0 +1,7 @@
+# Create a Salary Computation App
+# Create 3 Modules for this app
+# The first module is GrossSalary.py will handle the function for computing the gross salary.
+# The second module is SalaryDeductions.py, which will handle the function for computing the deductions
+# The last module NetSalary.py will be responsible for computing the net salary.
+# The user will input the following (Name, Hour, Loan, Health Insurance).
+# Tax(12% of the gross salary) and Rate(500/hr) is fixed

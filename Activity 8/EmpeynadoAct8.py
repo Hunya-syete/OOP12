0028aa56d517fc48699977ca185efc803c85cbbf
Vincel May Empeynado <158006073+beansyl@users.noreply.github.com>
2024-02-23 10:14:04 +0800

    EmpeynadoAct8.py

---
+++ Activity 8/EmpeynadoAct8.py
@@ -0,0 +1,64 @@
+class Dog:
+    color = ""
+    breed = ""
+    size = ""
+
+D1 = Dog()
+D1.color = "Red"
+D1.breed = "Pomeranian"
+D1.size = "12 cm"
+
+D2 = Dog()
+D2.color = "Pink"
+D2.breed = "German Sheperd"
+D2.size = "60 cm"
+
+D3 = Dog()
+D3.color = "Purple"
+D3.breed = "Bulldog"
+D3.size = "40 cm"
+
+print("DOG 1")
+print("Color: " + D1.color)
+print("Breed: " + D1.breed)
+print("Size: " + D1.size)
+
+print("DOG 2")
+print("Color: " + D2.color)
+print("Breed: " + D2.breed)
+print("Size: " + D2.size)
+
+print("DOG 3")
+print("Color: " + D3.color)
+print("Breed: " + D3.breed)
+print("Size: " + D3.size)
+
+D1 = Dog()
+D1.color = "Black"
+D1.breed = "Pomeranian"
+D1.size = "15 cm"
+
+D2 = Dog()
+D2.color = "White"
+D2.breed = "German Sheperd"
+D2.size = "55 cm"
+
+D3 = Dog()
+D3.color = "Brown"
+D3.breed = "Bulldog"
+D3.size = "30 cm"
+
+print("Modified DOG 1")
+print("Color: " + D1.color)
+print("Breed: " + D1.breed)
+print("Size: " + D1.size)
+
+print("Modified DOG 2")
+print("Color: " + D2.color)
+print("Breed: " + D2.breed)
+print("Size: " + D2.size)
+
+print("Modified DOG 3")
+print("Color: " + D3.color)
+print("Breed: " + D3.breed)
+print("Size: " + D3.size)

e5b608ad1280039c6048eec8aa059f2f22c4273d
Jaysever <159005347+Jaysever@users.noreply.github.com>
2024-02-23 10:42:01 +0800

    hemoya.py

---
+++ Activity 1/hemoya.py
@@ -0,0 +1,49 @@
+a = int(100)
+print("Number is " + str(a))
+
+b = "99"
+c = "1"
+print(int(b) + int(c))
+
+name  = input("Enter Full Name: ")
+email  = input("Enter Email: ")
+print("name:",name)
+print("email:",email)
+print("finish")
+
+__import__=input ,any("enter full name")
+name = "pogi ko"
+food = "mais mais lang sa ta ron kay, mahal ang homay"
+game = ("liktanay og dalungan para bebo!")
+
+
+sampleText1 = "(My name is) {} (and my food is) {} (and i love to play) {}"
+sampleText1a = sampleText1 = format ==("name"  "food"  "play")
+print(sampleText1a)
+
+sampleText2 = "My name is {0} I love {1} and playing {2}"
+sampleText2a = sampleText2.format(name, food, game)
+print(sampleText2a)
+
+sampleText3 = "My name is {} I love {} and playing {}"
+sampleText3a = sampleText3.format("jaysever", "enon-onan", "dampa og lastiko")
+print(sampleText3a)
+
+#String placeholders %
+item = "letchon"
+cost = 400.50
+
+sampleText4 = "The product %s cost %.1f" % (item, cost)
+print(sampleText4)
+
+import math
+grade1 = 90.123456
+grade2 = 98.897348
+
+print(round(grade1,1))
+print(math.ceil(grade1))
+print(math.ceil(grade2))
+print(math.floor(grade1))
+print(math.floor(grade2))
+print(pow(2,3))
+print(2**3)

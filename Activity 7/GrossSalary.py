293b029c56d2a9142a0f3f414786e2f7d41036b9
HaroldOnineza <158535536+HaroldOnineza@users.noreply.github.com>
2024-03-20 10:41:54 +0800

    Delete Activity 7/GrossSalary.py

--- Activity 7/GrossSalary.py
+++
@@ -1,9 +0,0 @@
-from SalaryDeduction import rate
-hours = int(input("Enter hour/s rendered: "))
-rate = 500
-
-def gross(hours, rate):
-    return int(hours) * int(rate)
-
-if hours >=1:
-    print("Gross Salary: Pesos " + str(gross(hours, rate)))

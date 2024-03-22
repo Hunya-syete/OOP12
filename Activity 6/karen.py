e3085ccba463a45fb7bf1185f0a167e682fe7849
karenkatecalunod <158007393+karenkatecalunod@users.noreply.github.com>
2024-03-22 12:18:28 +0800

    karen.py

---
+++ Activity 6/karen.py
@@ -0,0 +1,26 @@
+record = {}
+
+def add_record():
+    key = input("Enter Key: ")
+    value = input("Enter Value: ")
+    record[key] = value
+
+def delete_record():
+    key = input("Enter the Key you want to delete: ")
+    if key in record:
+        del record[key]
+        print(f"Key {key} deleted successfully.")
+    else:
+        print(f"Key {key} not found.")
+
+def main():
+    while True:
+        choice =\
+            input("Enter ADD data, DELETE data, END: ")
+        if choice == "ADD":
+            add_record()
+        elif choice == "DELETE":
+            delete_record()
+        elif choice == "END":
+            print("Thank you.")
+            break

7c7a4f69cc0e14ced26650f3d1b225a7383f5063
Jhon <158005993+banneee@users.noreply.github.com>
2024-04-15 11:28:35 +0800

    Add files via upload

---
+++ Activity 11/myproject(BICBIC NOT_FINAL)/myapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+# Redirect to a success page
+                return redirect('success')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def success(request):
+    return render(request, 'success.html')

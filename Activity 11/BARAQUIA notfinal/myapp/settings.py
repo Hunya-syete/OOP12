3807441b0f5372cde583c2118d4e9ed7b96834a7
JACKIEPALON123456789 <158006396+JACKIEPALON123456789@users.noreply.github.com>
2024-04-15 11:39:49 +0800

    Add files via upload

---
+++ Activity 11/BARAQUIA notfinal/myapp/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+ 'default': {
+   'ENGINE': 'django.db.backends.mysql',
+   'NAME': 'my database',
+   'USER': 'my database user',
+   'PASSWORD': 'my password',
+   'HOST': 'localhost',
+   'PORT': '3306',
+ }
+}

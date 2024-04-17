6f7c46b763c25817bada1e0743b86907984ccc60
richardmakahidhid <158006250+richardmakahidhid@users.noreply.github.com>
2024-04-17 15:28:59 +0800

    Add files via upload

---
+++ makahidhid.py/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+class LoginForm(forms.Form):
+ username = forms.CharField(label='Username', max_length=100)
+ password = forms.CharField(label='Password', widget=forms.PasswordInput)
---
+++ makahidhid.py/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from myproject.myapp.views import user_login,success
+urlpatterns = [
+       path('login/', user_login, name='login'),
+       path('success/', success, name='success')
+ ]

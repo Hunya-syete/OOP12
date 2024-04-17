b4253afdf1fbcda475cfe43da8e6db9811ccf022
jethroccrtz <158007669+jethroccrtz@users.noreply.github.com>
2024-04-17 11:34:19 +0800

    Add files via upload

---
+++ Activity 11/Cortez11(NF)/myproject/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from myapp.views import user_login, success
+
+urlpatterns= [
+    path('login/', user_login, name='login'),
+    path('success/', success, name='success')
+]

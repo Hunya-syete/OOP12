9d9fb3edb3815a0740034db2c3c167c349b48546
jethroccrtz <158007669+jethroccrtz@users.noreply.github.com>
2024-04-19 00:33:01 +0800

    Add files via upload

---
+++ Activity 10.2 (Code)/cortez10/address.py
@@ -0,0 +1,6 @@
+class Address:
+    def __init__(self, country, city, street, postal):
+        self.country = country
+        self.city = city
+        self.street = street
+        self.postal = postal

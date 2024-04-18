2fbf9045b0433244d4a7a90218f2fe3a693315d1
jethroccrtz <158007669+jethroccrtz@users.noreply.github.com>
2024-04-19 00:33:47 +0800

    Delete Activity 10/cortez10 directory

--- Activity 10/cortez10/address.py
+++
@@ -1,6 +0,0 @@
-class Address:
-    def __init__(self, country, city, street, postal):
-        self.country = country
-        self.city = city
-        self.street = street
-        self.postal = postal

829521207c963ebaaa26af73e276668e41fd15a8
jethroccrtz <158007669+jethroccrtz@users.noreply.github.com>
2024-04-19 11:35:11 +0800

    Delete Activity 11/Cortez11(NF) directory

--- Activity 11/Cortez11(NF)/myproject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from myapp.views import user_login, success
-
-urlpatterns= [
-    path('login/', user_login, name='login'),
-    path('success/', success, name='success')
-]
